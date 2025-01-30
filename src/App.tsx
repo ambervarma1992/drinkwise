e36b795b53a976853e2c9438da136677ed110046
Amber Varma <avarma@macbookpro.home>
2025-01-30 15:05:20 +0100

    Update tsconfig.json to fix React imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import { SessionManager } from './components/SessionManager';
 import { useSession } from './hooks/useSession';
